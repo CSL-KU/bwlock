5795aeaca0c465e75ec828a02d15d5b622e45c50
Waqar Ali <waqar.272@gmail.com>
2016-08-11 13:58:04 -0500

    Rectifying support for coarse-grain locking

--- src/user/coarse/bwlockset.c
+++ src/user/coarse/bwlockset.c
@@ -1,6 +1,12 @@
 #define _GNU_SOURCE         /* See feature_test_macros(7) */
+#include <unistd.h>
+#include <sys/syscall.h>   /* For SYS_xxx definitions */
+#include <stdio.h>
+#include <stdint.h>
+#include <time.h>
+#include <stdlib.h>
 
-#include "bwlock.h"
+#define SYS_bwlock 330
 
 void usage(char *argv[])
 {
@@ -11,7 +17,6 @@ void usage(char *argv[])
 int main(int argc, char *argv[])
 {
 	int pid, val;
-	int status = BW_SUCCESS;
 
 	if (argc != 3)
 		usage(argv);
@@ -19,26 +24,6 @@ int main(int argc, char *argv[])
 	pid = strtol(argv[1], NULL, 0);
 	val = strtol(argv[2], NULL, 0);
 
-	if (val == 0) {
-		/* This is a call for releasing bandwidth lock */
-		bw_unlock();
-		bw_unregister();
-	} else if (val == 1) {
-		/* Application wants to set bandwidth lock */
-		status = bw_register(pid);
-
-		if (status == BW_FAILURE) {
-			printf("Fatal Error : Bandwidth lock failed in memory-mapping stage\n");
-			return -1;
-		} else {
-			printf("set pid=%d val=%d\n", pid, val);
-			bw_lock();
-		}
-	} else {
-		/* Application has provided invalid value for bandwidth lock */
-		printf("Provided value (=%d) not recognized. Legal values are 0 <lock release> and 1 <lock set>\n", val);
-		return -1;
-	}
-		
-	return 0;
+	printf("set pid=%d val=%d\n", pid, val);
+	syscall(SYS_bwlock, pid, val);
 }
